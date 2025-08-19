8a0ec1c8da5c7317443edeee3e141f70cc8faebe
quentintyr <quentin.tyr@gmail.com>
2025-08-19 19:05:00 +0200

    🔨REF | simple file refactoring

--- src/main/java/dev/quentintyr/embellishedtooltips/Embellishedtooltips.java
+++ src/main/java/dev/quentintyr/embellishedtooltips/Embellishedtooltips.java
@@ -1,20 +1,19 @@
 package dev.quentintyr.embellishedtooltips;
 
-// import net.minecraftforge.api.distmarker.Dist;
-// import net.minecraftforge.fml.common.Mod;
-// import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class EmbellishedTooltips {
+public class EmbellishedTooltips implements ModInitializer {
     public static final String MODID = "embellished_tooltips";
     public static final Logger LOGGER = LogManager.getLogger("EmbellishedTooltips");
 
-    public EmbellishedTooltips() {
-        // ObscureTooltipsConfig.setup();
-        // if (FMLEnvironment.dist == Dist.CLIENT) {
-        // EmbellishedTooltipsClient.setup();
-        // }
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing Embellished Tooltips");
 
+        // Register common functionality here (if any)
+        // Most of our mod is client-side, so the main initialization happens in
+        // EmbellishedTooltipsClient
     }
 }
