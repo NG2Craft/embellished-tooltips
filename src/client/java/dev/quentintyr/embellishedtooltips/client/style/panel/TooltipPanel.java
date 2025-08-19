3aada69c510760868d90679f91da1706520af210
quentintyr <quentin.tyr@gmail.com>
2025-08-19 10:15:00 +0200

    🔨REF | update TooltipPanel for Fabric rendering - Update imports and package structure - Adapt to use DrawContext instead of GuiGraphics - Simplify panel rendering API

--- src/client/java/dev/quentintyr/embellishedtooltips/client/style/panel/TooltipPanel.java
+++ src/client/java/dev/quentintyr/embellishedtooltips/client/style/panel/TooltipPanel.java
@@ -1,16 +1,28 @@
-// package dev.quentintyr.embellishedtooltips.client.style.panel;
+package dev.quentintyr.embellishedtooltips.client.style.panel;
 
-// import dev.quentintyr.embellishedtooltips.client.renderer.TooltipContext;
-// import java.awt.Point;
-// import net.minecraft.world.phys.Vec2;
-// import net.minecraftforge.api.distmarker.Dist;
-// import net.minecraftforge.api.distmarker.OnlyIn;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
+import dev.quentintyr.embellishedtooltips.client.render.TooltipContext;
 
-// @OnlyIn(Dist.CLIENT)
-// @FunctionalInterface
-// public interface TooltipPanel {
-// void render(TooltipContext var1, Vec2 var2, Point var3, boolean var4);
+/**
+ * Represents a panel rendering style for tooltips.
+ */
+@Environment(EnvType.CLIENT)
+public abstract class TooltipPanel {
 
-// default void reset() {
-// }
-// }
+    /**
+     * Renders the tooltip panel.
+     *
+     * @param drawContext The DrawContext instance to render with.
+     * @param context     The tooltip context.
+     */
+    public abstract void render(DrawContext drawContext, TooltipContext context);
+
+    /**
+     * Gets the padding to apply to tooltip content.
+     *
+     * @return The padding as [left, top, right, bottom].
+     */
+    public abstract int[] getPadding();
+}
