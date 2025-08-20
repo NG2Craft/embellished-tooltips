9ec56d38b4af57d2bb63a304edd24a91b489a139
quentintyr <quentin.tyr@gmail.com>
2025-08-20 09:50:00 +0200

    🔨REF | implement client-side initialization - Add EmbellishedTooltipsClient with proper Fabric client mod initializer - Register ResourceLoader for tooltip resource management - Initialize StyleManager for tooltip styling system - Add basic EmbellishedTooltipsConfig structure for future configuration

--- src/client/java/dev/quentintyr/embellishedtooltips/EmbellishedTooltipsConfig.java
+++ src/client/java/dev/quentintyr/embellishedtooltips/EmbellishedTooltipsConfig.java
@@ -1,5 +1,12 @@
-// package dev.quentintyr.embellishedtooltips;
+package dev.quentintyr.embellishedtooltips;
 
-// public class EmbellishedTooltipsConfig {
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-// }
+/**
+ * Configuration for the mod.
+ */
+@Environment(EnvType.CLIENT)
+public class EmbellishedTooltipsConfig {
+    // Configuration options can be added here
+}
--- src/client/java/dev/quentintyr/embellishedtooltips/EmbellishedtooltipsClient.java
+++ src/client/java/dev/quentintyr/embellishedtooltips/EmbellishedtooltipsClient.java
@@ -1,23 +1,22 @@
 package dev.quentintyr.embellishedtooltips;
 
 import net.fabricmc.api.ClientModInitializer;
-// import dev.quentintyr.embellishedtooltips.client.ResourceLoader;
-// import net.minecraft.client.Minecraft;
-// import net.minecraft.server.packs.resources.ReloadableResourceManager;
-// import net.minecraft.server.packs.resources.ResourceManager;
+import dev.quentintyr.embellishedtooltips.client.ResourceLoader;
+import dev.quentintyr.embellishedtooltips.client.style.StyleManager;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
 
 public class EmbellishedTooltipsClient implements ClientModInitializer {
 	@Override
-	public void onInitializeClient() {
-
-	}
-
-	// public static void setup() {
-	// // ResourceManager var1 = Minecraft.m_91087_().m_91098_();
-	// // if (var1 instanceof ReloadableResourceManager) {
-	// // ReloadableResourceManager manager = (ReloadableResourceManager) var1;
-	// // manager.m_7217_(ResourceLoader.INSTANCE);
-	// // }
-
-	// }
-}
+    public void onInitializeClient() {
+        EmbellishedTooltips.LOGGER.info("Initializing Embellished Tooltips client");
+        
+        // Initialize style manager
+        StyleManager.getInstance();
+        
+        // Register resource loader for data pack resources
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
+            .registerReloadListener(ResourceLoader.INSTANCE);
+        
+        EmbellishedTooltips.LOGGER.info("Embellished Tooltips client initialized");
+    }
