865b6caefaf3e6f7064e54bd5aa8a19bc4894383
quentintyr <quentin.tyr@gmail.com>
2025-08-21 18:50:00 +0200

    ✨FEAT | implement tooltip rendering mixin for Fabric - Add GuiGraphicsMixin to intercept DrawContext tooltip rendering - Port from Forge GuiGraphics to Fabric DrawContext API - Enable custom tooltip rendering through TooltipRenderer integration - Support cancellable tooltip rendering for style system override - Map Forge tooltip method signatures to Fabric equivalents

--- src/client/java/dev/quentintyr/embellishedtooltips/mixin/client/GuiGraphicsMixin.java
+++ src/client/java/dev/quentintyr/embellishedtooltips/mixin/client/GuiGraphicsMixin.java
@@ -1,36 +1,30 @@
-// package dev.quentintyr.embellishedtooltips.mixin.client;
+package dev.quentintyr.embellishedtooltips.mixin.client;
 
-// // import dev.quentintyr.embellishedtooltips.client.renderer.TooltipContext;
-// // import dev.quentintyr.embellishedtooltips.client.renderer.TooltipRenderer;
-// // import java.util.List;
-// // import net.minecraft.client.gui.Font;
-// // import net.minecraft.client.gui.GuiGraphics;
-// // import
-// net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-// // import
-// net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
-// // import net.minecraft.world.item.ItemStack;
-// import org.spongepowered.asm.mixin.Mixin;
-// import org.spongepowered.asm.mixin.Shadow;
-// import org.spongepowered.asm.mixin.injection.At;
-// import org.spongepowered.asm.mixin.injection.Inject;
-// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import dev.quentintyr.embellishedtooltips.client.render.TooltipRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.client.gui.tooltip.TooltipPositioner;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// @Mixin(value = { GuiGraphics.class }, priority = 0)
-// public abstract class GuiGraphicsMixin {
-// @Shadow(remap = false)
-// private ItemStack tooltipStack;
+import java.util.List;
 
-// @Inject(method = { "renderTooltipInternal" }, at = { @At("HEAD") },
-// cancellable = true)
-// private void renderTooltip(Font font, List<ClientTooltipComponent>
-// components, int x, int y,
-// ClientTooltipPositioner positioner, CallbackInfo ci) {
-// if (TooltipRenderer.render(new TooltipContext((GuiGraphics) this),
-// this.tooltipStack, font, components, x, y,
-// positioner)) {
-// ci.cancel();
-// }
+@Mixin(DrawContext.class)
+public abstract class GuiGraphicsMixin {
+    @Shadow
+    private ItemStack tooltipStack;
 
-// }
-// }
+    @Inject(method = "drawTooltip(Lnet/minecraft/client/font/TextRenderer;Ljava/util/List;IILnet/minecraft/client/gui/tooltip/TooltipPositioner;)V", at = @At("HEAD"), cancellable = true)
+    private void renderTooltip(TextRenderer textRenderer, List<TooltipComponent> components,
+            int x, int y, TooltipPositioner positioner, CallbackInfo ci) {
+        if (TooltipRenderer.render((DrawContext) (Object) this, this.tooltipStack, textRenderer,
+                components, x, y, positioner)) {
+            ci.cancel();
+        }
+    }
+}
