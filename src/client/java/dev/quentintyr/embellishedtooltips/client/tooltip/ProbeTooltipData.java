0fcd516fe9e476bde1db82f05eec22f93eb557a2
quentintyr <quentin.tyr@gmail.com>
2025-08-28 18:10:00 +0200

    ♻️CHORE | remove unused files - Removed effects, readmes and unecessary code

--- src/client/java/dev/quentintyr/embellishedtooltips/client/tooltip/ProbeTooltipData.java
+++
@@ -1,8 +0,0 @@
-package dev.quentintyr.embellishedtooltips.client.tooltip;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
-public record ProbeTooltipData(String label) implements net.minecraft.client.item.TooltipData {
-}
